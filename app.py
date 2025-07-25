0455bb842aa7975718f870aa2308a246f2ce96b3
Kishore <112462625+Anonymous12k@users.noreply.github.com>
2025-07-25 16:14:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown(
 )
 
 # ------------------------ Title ------------------------
-st.title("(❁´◡`❁) Anime Emotion Recommender (Goth UI)")
+st.title("🎭Anime Emotion Recommender")
 st.write("Find anime based on how you feel. Pick your emotion and explore!")
 
 # ------------------------ Sidebar Filters ------------------------
